ef0ce92c984fa3ae4f8a1ae96c7d364535cd850c
Yohann <at.yohann@gmail.com>
2025-08-21 15:34:31 +0200

    add card projet

--- Pages/Portfolio.jsx
+++ Pages/Portfolio.jsx
@@ -1,28 +1,69 @@
 import React, { memo } from 'react';
-
+import courbe from '../src/assets/courbe.jpg';
+import ciseaux from '../src/assets/ciseaux.png';
+import calculatrice from '../src/assets/calculatrice.png';
+import morpion from '../src/assets/morpion.png';
+import manette from "../src/assets/manette.png";
 export default memo(function Portfolio() {
   return (
-    // <card numero 1>
-    <div
-      className="d-flex justify-content-center align-items-center min-vh-100"
-    >
-      <div className="card" style={{ width: '18rem' }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card’s content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+    <> <img className="img-fluid hero-img w-100" src={courbe} alt="image développeur web" />
+    
+    <div className="container my-5">
+      {/* Titre centré */}
+      <h1 className="text-center mb-4">Portfolio</h1>
+      <p className='text-center mb-'>Voici quelques projets que j'ai pu réaliser pour m'entrainer</p>
+
+      <div className="d-flex justify-content-center flex-wrap gap-4">
+        {/* Carte 1 */}
+        <div className="card" style={{ width: '18rem' }}>
+          <img src={morpion} alt="image de morpion" className="card-img-top" />
+          <div className="card-body">
+            <h5 className="card-title">Morpion</h5>
+            <p className="card-text">
+              Contenu de la carte numéro 1.
+            </p>
+            <a href="#" className="btn btn-primary">Voir plus</a>
+          </div>
         </div>
+
+        {/* Carte 2 */}
+        <div className="card" style={{ width: '18rem' }}>
+          <img src={ciseaux} alt="image de ciseaux" className="card-img-top" />
+          <div className="card-body">
+            <h5 className="card-title">Pierre Feuille Ciseaux</h5>
+            <p className="card-text">
+              Contenu de la carte numéro 2.
+            </p>
+            <a href="#" className="btn btn-primary">Voir plus</a>
+          </div>
+        </div>
+
+        {/* Carte 3 */}
+        <div className="card" style={{ width: '18rem' }}>
+          <img src={calculatrice} alt="image d'une calculatrice" className="card-img-top" />
+          <div className="card-body">
+            <h5 className="card-title">Calculatrice</h5>
+            <p className="card-text">
+              Contenu de la carte numéro 3.
+            </p>
+            <a href="#" className="btn btn-primary">Voir plus</a>
+          </div>
+        </div>
+
+        {/*Carte 4*/}
+        <div className="card" style={{ width: '18rem' }}>
+          <img src={manette} alt="image d'une manette " className="card-img-top" />
+          <div className="card-body">
+            <h5 className="card-title">Le juste Prix </h5>
+            <p className="card-text">
+              Contenu de la carte numéro 4.
+            </p>
+            <a href="#" className="btn btn-primary">Voir plus</a>
+          </div>
+        </div>
+
       </div>
     </div>
-    // card numero 2
-    
-    
+    </>
   );
-
 });
