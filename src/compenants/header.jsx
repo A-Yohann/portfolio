d5d289e63c8ee71ebaef7dad5a9980292fd50425
Yohann <at.yohann@gmail.com>
2025-11-24 16:29:25 +0100

    [NEW] add new navbar

--- src/compenants/header.jsx
+++ src/compenants/header.jsx
@@ -1,23 +1,23 @@
 // Header.js
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Header() {
   return (
-    <Navbar expand="lg" bg="light" variant="light" fixed="top">
+    <Navbar expand="lg" className="navbar-fun fixed-top">
       <Container fluid>
-        <Navbar.Brand as={NavLink} to="/">Yohann</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/" className="brand-fun">Yohann</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto ">
+          <Nav className="ms-auto nav-fun-links">
             <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
             <Nav.Link as={NavLink} to="/services">Services</Nav.Link>
             <Nav.Link as={NavLink} to="/portfolio">Portfolio</Nav.Link>
             <Nav.Link as={NavLink} to="/contact">Contact</Nav.Link>
             <Nav.Link as={NavLink} to="/mention">Mentions Légales</Nav.Link>
-            
           </Nav>
         </Navbar.Collapse>
       </Container>
